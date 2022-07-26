4b5a80c6bab35fb90048925b2d3032c2b25b8bce
Josh <josh@hessjd.com>
2022-07-26 14:46:22 -0500

    add contacts

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Window from '../components/Text/Window';
+import axios from 'axios';
+import { contactRoute } from '../utils/apiRoutes';
+import Contacts from '../components/Contacts/Contacts';
 
 function Chat() {
 
-  const logout = () => {
-    localStorage.removeItem('user');
-    
-  };
+	const [contacts, setContacts] = useState();
+    const [user, setUser] = useState();
 
-  return (
-    <div>
-      <Link to='/login' onClick={() => logout()}>Logout</Link>
-        <Window />
-    </div>
-  )
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!localStorage.getItem('user')) {
+        navigate('/login');
+      }
+      else {
+        setUser(localStorage.getItem('user')
+        )
+      }
+    }, [navigate]);
+
+	useEffect(() => {
+		if(user) {
+			axios.get(`${contactRoute}/${user._id}`)
+				.then((data) => {
+					setContacts(data.data);
+				})
+		}
+	}, [user])
+
+    const logout = () => {
+      localStorage.removeItem('user');
+    };
+
+    return (
+      <div>
+        <Link to='/login' onClick={() => logout()}>Logout</Link>
+			<Contacts />
+    		<Window />
+      </div>
+    )
 }
 
 export default Chat

ddad3b12c4fe094f2b49d0ff441ad079399cbacf
Josh <josh@hessjd.com>
2022-08-12 13:00:58 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const socket = require('socket.io');
 
 const app = express();
 const userRoutes = require('./routes/userRoutes');
@@ -15,7 +16,7 @@ app.use('/api/auth', userRoutes);
 app.use('/api/message', messageRoutes);
 
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
     console.log('Server started on port 5000.')
 })
 
@@ -26,4 +27,27 @@ mongoose.connect(MONGO_URL, {
     () => console.log('DB Connection Successful')
 ).catch(err => {
     console.log(`Database Connection Error: ${err.nessage}`)
+})
+
+const io = socket(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		credentials: true
+	}
+})
+
+global.online = new Map();
+
+io.on("connection", (socket) => {
+	global.chatSocket = socket;
+	socket.on('add-user', (user_id) => {
+		online.set(user_id, socket.id);
+	})
+
+	socket.on('send-message', (data) => {
+		const sendUserSocket = online.get(data.to);
+		if (sendUserSocket) {
+			socket.to(sendUserSocket).emit('msg-received', data.msg);
+		}
+	})
 })
